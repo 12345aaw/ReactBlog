fdc4af228a0f78d9fa5c0cec4d61cd7d805312eb
12345aaw <willis.andrew.a@gmail.com>
2018-01-03 02:46:46 -0500

    intial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class App extends React.Component {
+
+
+  render(){
+    return(
+      <BlogForm />>
+      <BlogList />>
+
+
+    );
+  }
+
+}
+
+export default App;
